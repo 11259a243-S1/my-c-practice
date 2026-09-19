a0d10093b89160ef54dfe8e6d6532e0f93cef1d5
11259a243-S1 <11259a243@kanchiuniv.ac.in>
2026-09-19 15:16:52 +0530

    Ex1.c.

---
+++ Ex2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main() {
+    int a[50],n,i,pos,v;
+    //Read size and elements
+    scanf("%d,&n");
+    for(i=0;i<n;i++);
+    scanf("%d",&a[i++]);
+    //Traverse
+    i=0;
+    while(i<n)
+    printf("%d",a[i++]);
+printf("\n");
+//Insert
+scanf("%d%d",&v,&pos);
+for(i=n-1;i>=pos;i--) a[i+1] = a[i];
+a[pos] = v;
+n++;
+//Display
+i=0;
+while(i<n)
+printf("%d",a[i++]);
+printf("/n");
+//Delete
+scanf("%d",&pos);
+for(i=pos;i<n-1;i++) a[i] = a[i+1];
+n--;
+//Display
+i=0;
+while(i,n)
+printf("%d",a[i++]);
+return 0;
+}
